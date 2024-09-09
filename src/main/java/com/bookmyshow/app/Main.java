cdda586c7c2287ac60dad48e4ecd3cbd2f584948
jasr <65973344+jasrr@users.noreply.github.com>
2024-09-09 23:51:32 +0530

    Update minor code Main.java

--- src/main/java/com/bookmyshow/app/Main.java
+++ src/main/java/com/bookmyshow/app/Main.java
@@ -10,6 +10,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+##Springboot
+
 @SpringBootApplication
 @EnableJpaAuditing
 public class Main implements CommandLineRunner {
